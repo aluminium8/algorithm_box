a433bad8bcc505cde3ed39b9cdcfb8578a9b3b67
aluminium8 <haneda.masaya1998@gmail.com>
2022-12-15 06:28:20 +0900

    cicd_test

---
+++ test_code_for_test_environment/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+void please_input_Hello_world(std::string input_s)
+{
+
+    try
+    {
+        if (input_s != "Hello_world")
+        {
+            throw "input string is not Hello_world!! this is " + input_s;
+        }
+        else
+        {
+            std::cout << input_s << " this is great string!" << std::endl;
+        }
+    }
+    catch (char *e)
+    {
+        std::cout << e << '\n';
+    }
+}
+
+int main()
+{
+
+    std::cout << "first, I'LL input correct string" << std::endl;
+    please_input_Hello_world("Hello_world");
+
+    std::cout << "second, I'LL input mistaken string" << std::endl;
+    please_input_Hello_world("Hello world");
+    std::cout << "program will end" << std::endl;
+}
