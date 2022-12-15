4665d370cbe7444c1c0aaa7146403405fdf1ec4c
aluminium8 <haneda.masaya1998@gmail.com>
2022-12-15 18:22:57 +0900

    add cpp review folder

--- cpp/review_about_cpp/cpp_random_test/main.cpp
+++ cpp/review_about_cpp/cpp_random_test/main.cpp
@@ -16,7 +16,7 @@ int main()
     std::cout << "|---------------------|";
     for (int i = 0; i < 10; i++)
     {
-        std::cout << i << "--------------- | ";
+        std::cout <<  "--------------- | ";
     }
     std::cout << std::endl;
 
