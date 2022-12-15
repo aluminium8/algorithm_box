56f0f9e06480ec2b0fcecdb0106f0d0dd0ec19b0
aluminium8 <haneda.masaya1998@gmail.com>
2022-12-15 18:24:06 +0900

    add cpp review folder

--- cpp/review_about_cpp/cpp_random_test/main.cpp
+++ cpp/review_about_cpp/cpp_random_test/main.cpp
@@ -6,14 +6,14 @@ int main()
     std::cout << "# random number generator" << std::endl;
 
     std::mt19937 engine_A1(1), engine_A2(1), engine_B1(2);
-    std::cout << " | engine is mt19937";
+    std::cout << " | engine is mt19937 |";
     for (int i = 0; i < 10; i++)
     {
         std::cout << i << " | ";
     }
     std::cout << std::endl;
 
-    std::cout << "|---------------------|";
+    std::cout << "| --------------------- |";
     for (int i = 0; i < 10; i++)
     {
         std::cout <<  "--------------- | ";
