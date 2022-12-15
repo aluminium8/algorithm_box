f38d000326ef8136e4a60976ef451182e0ff66f7
aluminium8 <haneda.masaya1998@gmail.com>
2022-12-15 18:21:47 +0900

    add cpp review folder

--- cpp/review_about_cpp/cpp_random_test/main.cpp
+++ cpp/review_about_cpp/cpp_random_test/main.cpp
@@ -3,23 +3,42 @@
 
 int main()
 {
-    std::cout<<"# random number generator"<<std::endl;
+    std::cout << "# random number generator" << std::endl;
 
     std::mt19937 engine_A1(1), engine_A2(1), engine_B1(2);
+    std::cout << " | engine is mt19937";
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << " | ";
+    }
+    std::cout << std::endl;
 
-    for (int i = 1; i < 10; i++)
+    std::cout << "|---------------------|";
+    for (int i = 0; i < 10; i++)
     {
-        std::cout << engine_A1() << " ";
+        std::cout << i << "--------------- | ";
     }
     std::cout << std::endl;
-    for (int i = 1; i < 10; i++)
+
+    std::cout << " | seed is 1";
+    for (int i = 0; i < 10; i++)
     {
-        std::cout << engine_A2() << " ";
+        std::cout << engine_A1() << " | ";
     }
     std::cout << std::endl;
-    for (int i = 1; i < 10; i++)
+
+    std::cout << " | seed is 1";
+    for (int i = 0; i < 10; i++)
     {
-        std::cout << engine_B1() << " ";
+        std::cout << engine_A2() << " | ";
+    }
+    std::cout << std::endl;
+
+    
+    std::cout << " | seed is 2";
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << engine_B1() << " | ";
     }
     std::cout << std::endl;
 }
